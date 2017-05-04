451e95d5ce2feb446490342eea45cde10e201390
Matt Gardner <matthewlgardner@gmail.com>
2017-05-03 22:02:07 -0600

    add updateButton function to toggle play/pause button

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
 window.onload = function() {
 
+// Get Our Elements
+const player = document.querySelector('.player')
+const video = player.querySelector('.viewer')
+const progress = player.querySelector('.progress')
+const progressBar = player.querySelector('.progress__filled')
+const toggle = player.querySelector('.toggle')
+const skipButtons = player.querySelector('[data-skip]')
+const ranges = player.querySelector('.player__slider')
+
+// Build Our Functions
+
+function togglePlay() {
+  if (video.paused) {
+    video.play()
+  } else {
+    video.pause()
+  }
+}
+
+function updateButton() {
+  let icon = this.paused ? '►' : '❚ ❚'
+  console.log(icon);
+  toggle.textContent = icon
+}
+
+
+// Hook up the event listeners
+video.addEventListener('click', togglePlay)
+video.addEventListener('play', updateButton)
+video.addEventListener('pause', updateButton)
+toggle.addEventListener('click', togglePlay)
+
 
 
 }
