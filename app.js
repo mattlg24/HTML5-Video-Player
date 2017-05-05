22f1e92ff47f8b178c8a6eb361d56572942e5e5b
Matt Gardner <matthewlgardner@gmail.com>
2017-05-04 21:49:45 -0600

    add skip function

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const video = player.querySelector('.viewer')
 const progress = player.querySelector('.progress')
 const progressBar = player.querySelector('.progress__filled')
 const toggle = player.querySelector('.toggle')
-const skipButtons = player.querySelector('[data-skip]')
-const ranges = player.querySelector('.player__slider')
+const skipButtons = player.querySelectorAll('[data-skip]')
+const ranges = player.querySelectorAll('.player__slider')
 
 // Build Our Functions
 
@@ -21,17 +21,22 @@ function togglePlay() {
 
 function updateButton() {
   let icon = this.paused ? '►' : '❚ ❚'
-  console.log(icon);
   toggle.textContent = icon
 }
-
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip)
+}
 
 // Hook up the event listeners
 video.addEventListener('click', togglePlay)
 video.addEventListener('play', updateButton)
 video.addEventListener('pause', updateButton)
+
 toggle.addEventListener('click', togglePlay)
 
+skipButtons.forEach(button => {
+  button.addEventListener('click', skip)
+})
 
 
 }
